0f6263689a04a685070d8912eb6108bfd6c33201
mohammb04 <mohammb@hiof.no>
2024-03-19 18:56:15 +0100

    Test
    Byttet ut all html i app.js og all css i app.css med min forrige innlevering

--- our-react-app/src/App.js
+++ our-react-app/src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We straight crippin
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+        <nav>
+            <ul>
+                <li id="active">HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Sanity and headless CMS</li>
+            </ul>
+        </nav>
+        <main>
+            <article>
+                <h2>Test</h2>
+                <p id="tekst">HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.</p>
+                <ul>
+                    <li><a href="#">Test</a></li>
+                    <li><a href="#">Test</a></li>
+                    <li><a href="#">Test</a></li>
+                </ul>
+            </article>
+        </main>
     </div>
   );
 }
