ff1fc8e43e024ad0607726a52cac39ebe92a28ae
WarisTheDon <99509063+WarisTheDon@users.noreply.github.com>
2024-03-19 20:24:36 +0100

    java.js endringering
    fjerna html strkturen vår og la til en annen, kan fjerne dette ettersom det ikke egentelig funker.

--- our-react-app/src/App.js
+++ our-react-app/src/App.js
@@ -1,29 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/main.css';
+import { hentAlleVarer } from './sanity/service';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
-    <div id="container">
-        <nav>
-            <ul>
-                <li id="active">HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Sanity and headless CMS</li>
-            </ul>
-        </nav>
-        <main>
-            <article>
-                <h2>Test</h2>
-                <p id="tekst">HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.</p>
-                <ul>
-                    <li><a href="#">Test</a></li>
-                    <li><a href="#">Test</a></li>
-                    <li><a href="#">Test</a></li>
-                </ul>
-            </article>
-        </main>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          We straight crippin
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
