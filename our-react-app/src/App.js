54b01a0413d4f5ec6ac302bc8d980564827d7d96
mohammb04 <mohammb@hiof.no>
2024-03-21 21:35:40 +0100

    siste steg i oppgaven
    Gjorde det siste steg i oppgaven med beskrivelse: "Sett opp Routing til sidene /html, /css, /javascript, /react og /sanity, hvor hver av disse router til componenten "Resouces", og sender med en prop til "Resources" med riktig ressurskategori ( eks: category={"html"} ). Bruk prop-en "category" og moderne javascript for å liste opp riktige ressurser fra ressurser.js.".
    
    Dette ble gjort fordi jeg ikke kunne kjøre nettsiden av en eller annen grunn så pusher dette i et forsøk for å se om endringene jeg gjorde fungerte og kan syntes på en annen pc

--- our-react-app/src/App.js
+++ our-react-app/src/App.js
@@ -1,33 +1,44 @@
-import React from 'react'; // Legg til React import
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import ResourcePage from './resourcePage';
+import resourcesData from './resources';
 
-import './css/sass/main.scss'; // Importer SCSS-filen
-
-
+import './css/sass/main.scss';
 
 function App() {
   return (
-    <div id="container">
+    <Router>
+      <div id="container">
         <nav>
-            <ul>
-                <li id="active">HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Sanity and headless CMS</li>
-            </ul>
+          <ul>
+            <li><Link to="/html">HTML</Link></li>
+            <li><Link to="/css">CSS</Link></li>
+            <li><Link to="/javascript">JavaScript</Link></li>
+            <li><Link to="/react">React</Link></li>
+            <li><Link to="/sanity">Sanity and headless CMS</Link></li>
+          </ul>
         </nav>
         <main>
-            <article>
-                <h2>Test</h2>
-                <p id="tekst">HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.</p>
-                <ul>
-                    <li><a href="#">Test</a></li>
-                    <li><a href="#">Test</a></li>
-                    <li><a href="#">Test</a></li>
-                </ul>
-            </article>
+          <Switch>
+            <Route path="/html">
+              <ResourcePage category="HTML" resources={resourcesData} />
+            </Route>
+            <Route path="/css">
+              <ResourcePage category="CSS" resources={resourcesData} />
+            </Route>
+            <Route path="/javascript">
+              <ResourcePage category="JavaScript" resources={resourcesData} />
+            </Route>
+            <Route path="/react">
+              <ResourcePage category="React" resources={resourcesData} />
+            </Route>
+            <Route path="/sanity">
+              <ResourcePage category="Sanity and headless CMS" resources={resourcesData} />
+            </Route>
+          </Switch>
         </main>
-    </div>
+      </div>
+    </Router>
   );
 }
 
